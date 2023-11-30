23edff0fe07fae194ae6fa710927f80e606835d5
ommer1321 <1453o.faruk@gmail.com>
2023-11-30 19:45:56 +0300

    kurulum with ef

---
+++ CleanArchitecture.Presentation/Controllers/ValuesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchitecture.Presentation.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public sealed class ValuesController :ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("bu denemedir");
+        }
+    }
+}
